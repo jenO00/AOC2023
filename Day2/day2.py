27c24381bb65bf27ad2656ad9bbff017019e09a3
jenO00 <86131126+jenO00@users.noreply.github.com>
2023-12-06 12:39:28 +0100

    Day 2 done.

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,91 +1,163 @@
 def readFile():
-    #read the file
-    moves = []
+    lines = []
     with open('day2.txt') as file:
         for line in file:
-            moves.append(line.rstrip())
-    return moves
-            
+            lines.append((line.strip()).lower())
+    #print(lines)
+    return lines
 
-def rpsStrategy():
-    #read the lines
-    moves = readFile()
-    sumOfMoves = 0
-    #Rules as follow:
-    #A = rock
-    #B = paper
-    #C = scissor
-    #X = rock, Y = paper, Z = scissor
-    #Points: 1 for rock, 2 for paper, 3 for scissors
-    #Outcome: 0 if lost, 3 is draw, 6 if won
-    for move in moves:
-        if move[0] == "A" and move[2] == "X":
-            #Rock and Rock, 1+3, rock+draw
-            sumOfMoves+=4
-        elif move[0] == "A" and move[2] == "Y":
-            #rock and paper (2+6)
-            sumOfMoves+=8
-        elif move[0] == "A" and move[2] == "Z":
-            #rock and scissors
-            sumOfMoves+=(3+0)
-        elif move[0] == "B" and move[2]=="X":
-            #paper and rock 1+0
-            sumOfMoves+=(1+0)
-        elif move[0] == "B" and move[2] == "Y":
-            #paper and paper #2+3
-            sumOfMoves+=(2+3)
-        elif move[0]=="B" and move[2]=="Z":
-            sumOfMoves+=(3+6)
-        elif move[0]=="C" and move[2]=="X":
-            #scissors and rock
-            sumOfMoves+=(1+6)
-        elif move[0]=="C" and move[2]=="Y":
-            #scissors and paper
-            sumOfMoves += (2+0)
+#Part1
+def possibleGames():
+    listgames = readFile()
+    colon = ":"
+    comma = ","
+    semi = ";"
+    IDCount = 0
+    correctIDS = 0
+    yesno= []
+    testData = ['Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+        'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue','Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+        'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red','Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green']
+    games = []
+    for game in listGames:
+        print("index: ", listgames.index(game))
+        if listgames.index(game) < 99:
+            games.append(game[8:].rstrip(''))
         else:
-            #scissors and scissors
-            sumOfMoves +=(3+3)
-    return sumOfMoves
-
-def partTwo():
-    moves = readFile()
-    #A = Rock, B = Paper, C = Scissors
-    #X = Lose, Y = Draw, Z = Win
-    sumOfMoves = 0
-    for move in moves:
-        if move[2] == "X":
-            #Has to lose
-            if move[0] == "A":
-                #rock and lose
-                #choose scissors
-                sumOfMoves+=(3+0)
-            if move[0] == "B":
-                #they have paper, we choose rock
-                sumOfMoves += (1+0)
-            if move[0] == "C":
-                #they have scissors, we choose paper
-                sumOfMoves += (2+0)
-        if move[2]=="Y":
-            #Create a draw
-            if move[0] == "A":
-                #rock and rock
-                sumOfMoves+=(1+3)
-            if move[0]=="B":
-                #paper and paper
-                sumOfMoves+=(2+3)
-            if move[0] == "C":
-                #scissors and scissors
-                sumOfMoves+=(3+3)
-        if move[2]=="Z":
-            #must win
-            if move[0] == "A":
-                #rock and paper
-                sumOfMoves+=(2+6)
-            if move[0]=="B":
-                #paper and scissors
-                sumOfMoves+=(3+6)
-            if move[0] == "C":
-                #scissors and rock
-                sumOfMoves+=(1+6)
-    return sumOfMoves
-    
+            games.append(game[9:].rstrip(''))
+    for i in games:
+        IDCount += 1
+        number = ""
+        blueCount = 0
+        redCount = 0
+        greenCount = 0
+        color = ""
+        for char in i:
+            if char.isdigit():
+                #if it is a number
+                number += char
+           
+            elif char == semi:
+                #new game
+                blueCount = 0
+                greenCount = 0
+                redCount = 0
+                number = ""
+                print("NEW GAME, IDCOUNT: ", IDCount)
+            elif char != comma and char != " " and not char.isdigit():
+                color += char
+                if color == "blue":
+                    blueCount += int(number)
+                    if blueCount > 14:
+                        yesno.append("no")
+                    else:
+                        yesno.append("yes")
+                    print("Blue: ", blueCount)
+                    number = "" #nollställ
+                    color = ""
+                elif color == "green":
+                    greenCount += int(number)
+                    print("Green: ", greenCount)
+                    if greenCount > 13:
+                        yesno.append("no")
+                    else:
+                        yesno.append("yes")
+                    number = ""
+                    color = ""
+                elif color == "red":
+                    redCount += int(number)
+                    print("Red: ", redCount)
+                    if redCount > 12:
+                        yesno.append("no")
+                    else:
+                        yesno.append("yes")
+                    number = ""
+                    color = ""
+                else:
+                    pass
+        print("Yesno: ", yesno)
+        if "no" in yesno:
+            yesno = [] #återställ fuck den
+        else:
+            correctIDS += IDCount
+            yesno = [] #återställ
+    return correctIDS
+        
+        
+def leastNeeded():
+    listgames = readFile()
+    colon = ":"
+    comma = ","
+    semi = ";"
+    IDCount = 0
+    correctIDS = 0
+    yesno= []
+    res = 0
+    testData = ['Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+        'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue','Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+        'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red','Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green']
+    games = []
+    for game in listgames:
+         if listgames.index(game) < 99:
+             games.append(game[8:].rstrip(''))
+         else:
+             games.append(game[9:].rstrip(''))
+    biggestBlue = 0
+    biggestRed = 0
+    biggestGreen = 0
+    for i in games:
+        res += biggestRed*biggestGreen*biggestBlue
+        print("res: ", res)
+        IDCount += 1
+        #loop through each
+        number = ""
+        blueCount = 0
+        redCount = 0
+        greenCount = 0
+        biggestBlue = 0
+        biggestRed = 0
+        biggestGreen = 0
+        color = ""
+        for char in i:
+            if char.isdigit():
+                #if it is a number
+                number += char
+           
+            elif char != comma and char != " " and not char.isdigit() and char != semi:
+                color += char
+             #   print("Color: ", color)
+              #  print("Color: ", color)
+                if color == "blue":
+                    blueCount += int(number)
+                    if blueCount > biggestBlue:
+                        biggestBlue = blueCount
+                     
+                    number = "" #nollställ
+                    color = ""
+                elif color == "green":
+                    greenCount += int(number)
+                    if greenCount > biggestGreen:
+                        biggestGreen = greenCount
+                    
+                    number = ""
+                    color = ""
+                elif color == "red":
+                    redCount += int(number)
+                    if redCount > biggestRed:
+                        biggestRed = redCount
+        
+                    number = ""
+                    color = ""
+                else:
+                    pass
+            elif char == semi:
+                #new game
+              
+                blueCount = 0
+                greenCount = 0
+                redCount = 0
+                number = ""
+                print("NEW GAME, IDCOUNT: ", IDCount)
+    res += biggestRed*biggestGreen*biggestBlue
+    print("res: ", res)
+    return res     
